87a81ac588747634ef23f6f60a217e724ecbd37f
vinylguy <vinylguy@fullsail.edu>
2012-04-04 15:24:10 -0700

    added while loop for preheating oven to 375

--- baker_nathan_assignment2.js
+++ baker_nathan_assignment2.js
@@ -1,2 +1,55 @@
-alert("JavaScript works!");
+/*
+Nathan Baker
+04/04/2012
+Deliverable 2
+Cooking "Theme"
+*/
+
+var ingredients = ["pork", "shallots", "beer", "garlic", "spices"];
+var temp = 0
+
+
+
+while (temp <= 375) {
+	console.log("Pre-heating oven to 375f, current temp = " + temp + "f");
+	temp+=5;
+};
+console.log("Oven is hot and ready to go");
+
 console.log("Added line of code in my branch");
+
+
+
+/* var chef = "Nathan Baker"
+	recipe = '"Famous Pork Ribs"',
+	temp = 375,
+	tender = true,
+	ingredients = true
+;	
+
+console.log("Let's get started making a great meal, Nates " + recipe);
+console.log("Our chef today is Nathan Baker", "and I'm going to walk you through making my ribs.");
+
+if (chef === "Nathan Baker") {
+	console.log("You may proceed with the recipe.");
+	if (ingredients === true) {
+		console.log("Looks like we have all of our ingredients.",
+			"Sweet, now we can start to cook our " + recipe);
+		if (temp >= 375) {
+			console.log("Oven is hot and ready to go. Cook for 3 hours or until tender.");
+			if (tender === true) {
+				console.log("Smother ribs in bbq sauce and enjoy!");
+			} else {
+				console.log("Put back in the oven to cook longer.");
+			};
+		} else {
+			console.log("Preheat oven to 375f.");
+		};
+	} else {
+		console.log("Darn it, back to the store.");
+	};
+} else {
+	console.log("Stop! Only Nathan Baker is allowed to make these!");
+};
+
+*/
