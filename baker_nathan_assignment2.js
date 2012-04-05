a624d0162941c8d4759ee1546f7f3f75a31b4740
vinylguy <vinylguy@fullsail.edu>
2012-04-05 16:19:25 -0700

    Added boolean comparsion function

--- baker_nathan_assignment2.js
+++ baker_nathan_assignment2.js
@@ -38,39 +38,18 @@ say(ovenTemp);
 say("Put pan of ingredients into the oven and bake for 3 hours");
 
 //Boolean comparison
-var tender = [true, false];
-
-
-/* var chef = "Nathan Baker"
-	recipe = '"Famous Pork Ribs"',
-	temp = 375,
-	tender = true,
-	ingredients = true
-;	
-
-console.log("Let's get started making a great meal, Nates " + recipe);
-console.log("Our chef today is Nathan Baker", "and I'm going to walk you through making my ribs.");
-
-if (chef === "Nathan Baker") {
-	console.log("You may proceed with the recipe.");
-	if (ingredients === true) {
-		console.log("Looks like we have all of our ingredients.",
-			"Sweet, now we can start to cook our " + recipe);
-		if (temp >= 375) {
-			console.log("Oven is hot and ready to go. Cook for 3 hours or until tender.");
-			if (tender === true) {
-				console.log("Smother ribs in bbq sauce and enjoy!");
-			} else {
-				console.log("Put back in the oven to cook longer.");
-			};
-		} else {
-			console.log("Preheat oven to 375f.");
-		};
-	} else {
-		console.log("Darn it, back to the store.");
+var isTender = function(temp, time) {
+	if (temp>=375 && time>=3) {
+	var cooked = "The ribs cooked at " + temp + "f, for " + time + "hours, and are ready to sauce.";
+	return cooked
+	}; 
+		if (temp<375 || time<3);
+		{
+		var unCooked = "The ribs are not finished cooking"
+		return unCooked
 	};
-} else {
-	console.log("Stop! Only Nathan Baker is allowed to make these!");
 };
 
-*/
+var cooked = isTender(380, 4);
+say(cooked || unCooked);
+
